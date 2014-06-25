971fe7dac827f8f2632b803b0dadf87c6bbd8f96
Philip Johnson <philipbrucejohnson@gmail.com>
2014-06-25 08:37:43 -0400

    Update hello.js

--- js/hello.js
+++ js/hello.js
@@ -2,29 +2,18 @@ define(function (require) {
   return {
 
     model: {
-      state_code: ""
+      stateCode: ""
     },
 
     view: function () {/*
-      <div class="jumbotron">
-        <h1>Hello, world!</h1>
-        <p>Please enter a two letter state code
-            and we will look it up for you.</p>
-        <p>
-          <input class="hello-form" type="text" name="state_code"
-              value="< json.state_code >" />
-        </p>
-        <p>
-          <a href="#" class="btn btn-primary hello-click" role="button">
-            < "Search" >
-          </a>
-        </p>
-      </div>
+      <p>Please enter a two letter state code
+          and we will look it up for you.</p>
+      <input class="hello-form" type="text" name="stateCode"
+          value="< json.stateCode >" />
+      <button class="hello-click">Search</button>
     */},
 
     controller: function () {
-      var $ = require("jquery");
-      var streamline = require("streamline");
       $(".hello-click").click(function () {
         streamline("world", "php", "hello-form", "main-div");
       });
