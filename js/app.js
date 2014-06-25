9f70b67908d7b72a24a59c460f9efedf05884715
Philip Johnson <philipbrucejohnson@gmail.com>
2014-06-24 21:35:51 -0400

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,54 @@
+define(function (require) {
+  return {
+
+    model: {
+    },
+
+    view: function () {/*
+      <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle"
+                data-toggle="collapse" data-target=".navbar-collapse">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Bootstrap theme</a>
+          </div>
+          <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#contact">Contact</a></li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                  < "Dropdown " >
+                  <b class="caret"></b>
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Action</a></li>
+                  <li><a href="#">Another action</a></li>
+                  <li><a href="#">Something else here</a></li>
+                  <li class="divider"></li>
+                  <li class="dropdown-header">Nav header</li>
+                  <li><a href="#">Separated link</a></li>
+                  <li><a href="#">One more separated link</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div><br /><br /><br /></div>
+      <div id="main-div" class="container theme-showcase" role="main"></div>
+    */},
+
+    controller: function () {
+      var streamline = require("streamline");
+      streamline("hello", "", "", "main-div");
+    }
+
+  };
+});
