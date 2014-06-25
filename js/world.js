33d2a900abb72ba06b40be4c9a88da0dfbc15b84
Philip Johnson <philipbrucejohnson@gmail.com>
2014-06-25 08:38:16 -0400

    Update world.js

--- js/world.js
+++ js/world.js
@@ -2,25 +2,19 @@ define(function (require) {
   return {
 
     model: {
-      state_name: ""
+      stateName: ""
     },
 
     view: function () {/*
-      <div class="panel-body">
-        <h3 class="text-info">
-          < "That would be " + json.state_name + "." >
-        </h3>
-        <p>The state name was returned by the world.php script.</p>
-        <p>When you click the Back button, notice that the state
-            <br>code will be prepopulated with your previous entry.</p>
-        <p><button class="btn btn-lg btn-default world-click"
-            type="button">Back</button></p>
-      </div>
+      <h3>Hello < json.stateName >!</h3>
+      <p>The state name was returned by the world.php script
+          <br>which is located in the php folder.</p>
+      <p>When you click the Back button, notice that the state
+          <br>code will be prepopulated with your previous entry.</p>
+      <button class="world-click">Back</button>
     */},
 
     controller: function () {
-      var $ = require("jquery");
-      var streamline = require("streamline");
       $(".world-click").click(function () {
         streamline("hello", "", "", "main-div");
       });
